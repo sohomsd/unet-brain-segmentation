235898ff55571b8c23cb9e09eeedf62933869cef
sohomsd <sohomdash01@gmail.com>
2024-04-26 11:50:25 -0500

    Integrated skip connections in DecodeBlock for BaseUnet

--- models.py
+++ models.py
@@ -1,6 +1,20 @@
 import torch
 import torch.nn as nn
+from torchvision.transforms.functional import center_crop
 from model_components import *
+
+class DecodeBlock(nn.Module):
+    def __init__(self, in_ch, out_ch):
+        super(DecodeBlock, self).__init__()
+
+        self.upconv = nn.ConvTranspose2d(in_channels=in_ch, out_channels=out_ch, kernel_size=2, stride=2)
+        self.convblock = ConvBlock(in_ch=in_ch, out_ch=out_ch, kernel_size=3)
+
+    def forward(self, x, skip):
+        out = self.upconv(x)
+        cropped_skip = center_crop(skip, output_size=out.shape[-2:])
+
+        return self.convblock(torch.cat([out, cropped_skip], 1))
 
 
 class BaseUnet(nn.Module):
@@ -20,32 +34,25 @@ class BaseUnet(nn.Module):
         self.bridge = ConvBlock(in_ch=512, out_ch=1024, kernel_size=3)
 
         # Decode layers on Unet ascent
-        self.upconv1 = nn.ConvTranspose2d(in_channels=1024, out_channels=512, kernel_size=2, stride=2)
-        self.decode1 = ConvBlock(in_ch=1024, out_ch=512, kernel_size=3)
-
-        self.upconv2 = nn.ConvTranspose2d(in_channels=512, out_channels=256, kernel_size=2, stride=2)
-        self.decode2 = ConvBlock(in_ch=512, out_ch=256, kernel_size=3)
-
-        self.upconv3 = nn.ConvTranspose2d(in_channels=256, out_channels=128, kernel_size=2, stride=2)
-        self.decode3 = ConvBlock(in_ch=256, out_ch=128, kernel_size=3)
-
-        self.upconv4 = nn.ConvTranspose2d(in_channels=128, out_channels=64, kernel_size=2, stride=2)
-        self.decode4 = ConvBlock(in_ch=128, out_ch=64, kernel_size=3)
+        self.decode1 = DecodeBlock(in_ch=1024, out_ch=512)
+        self.decode2 = DecodeBlock(in_ch=512, out_ch=256)
+        self.decode3 = DecodeBlock(in_ch=256, out_ch=128)
+        self.decode4 = DecodeBlock(in_ch=128, out_ch=64)
 
         # Output 1x1 Convolution
         self.outconv = nn.Conv2d(in_channels=64, out_channels=out_ch, kernel_size=1)
 
     def forward(self, x):
-        skip1 = self.maxpool(self.encode1(x))
-        skip2 = self.maxpool(self.encode2(skip1))
-        skip3 = self.maxpool(self.encode3(skip2))
-        skip4 = self.maxpool(self.encode4(skip3))
+        skip1 = self.encode1(x)
+        skip2 = self.encode2(self.maxpool(skip1))
+        skip3 = self.encode3(self.maxpool(skip2))
+        skip4 = self.encode4(self.maxpool(skip3))
 
-        out = self.bridge(skip4)
+        out = self.bridge(self.maxpool(skip4))
 
-        out = self.decode1(torch.cat([out, skip4]))
-        out = self.decode2(torch.cat([out, skip3]))
-        out = self.decode3(torch.cat([out, skip2]))
-        out = self.decode4(torch.cat([out, skip1]))
+        out = self.decode1(out, skip4)
+        out = self.decode2(out, skip3)
+        out = self.decode3(out, skip2)
+        out = self.decode4(out, skip1)
 
         return self.outconv(out)
