f4a7728c6091252ab37b1463693d95ac4be99f25
sohomsd <sohomdash01@gmail.com>
2024-04-28 00:07:56 -0500

    Added padding option to ConvBlock to allow for same-padding

--- unet_models/base.py
+++ unet_models/base.py
@@ -4,13 +4,13 @@ from torchvision.transforms.functional import center_crop
 
 # Convolution Block with 2 Convolution and ReLU Activations
 class ConvBlock(nn.Module):
-    def __init__(self, in_ch, out_ch, kernel_size, stride=1):
+    def __init__(self, in_ch, out_ch, kernel_size, stride=1, padding=1):
         super(ConvBlock, self).__init__()
         self.conv1 = nn.Conv2d(in_channels=in_ch, out_channels=out_ch,
-                               kernel_size=kernel_size, stride=stride)
+                               kernel_size=kernel_size, stride=stride, padding=padding)
         self.relu1 = nn.ReLU()
         self.conv2 = nn.Conv2d(in_channels=out_ch, out_channels=out_ch, 
-                               kernel_size=kernel_size, stride=stride)
+                               kernel_size=kernel_size, stride=stride, padding=padding)
         self.relu2 = nn.ReLU()
 
     def forward(self, x):
